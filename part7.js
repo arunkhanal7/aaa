6885835f572fc3fefd677f1f5c04db855aece58c
Arun <kingarun2020@gmail.com>
2023-07-05 15:34:38 +0545

    revision

--- part7.js
+++ part7.js
@@ -2,7 +2,7 @@
 
 // for (let i = 0; i < num.length; i++) {
 //     console.log(num[i]);
-    
+
 // }
 
 //for each loop in arrays
@@ -30,10 +30,9 @@
 // let num =[3,5,1,2,4]
 // let a = num.map((value,index,array)=>{
 //     console.log(value,index,array)
-//     return value + index; //it creates new array 
+//     return value + index; //it creates new array
 // })
 // console.log(a)
-
 
 //array filter method : filters an array that passes the test and also creates new array
 // let arr=[45,23,21,0,3,5]
@@ -81,10 +80,41 @@
 // })
 // console.log(n)
 
-
 //Qno 5: use reduce to calculate factorial of a given number from a array
 // let arr=[1,2,3,4]
 // let n= arr.reduce((v1,v2)=>{
 //     return v1*v2
 // })
 // console.log(n)
+
+//exercise 1
+//write a javascript program to generate a random number and store it in a variable.The Program then takes an input from the user to tell them whether the guess was correct, greater or lesser than the original number.100 - no of guesses is score .program should is expected to terminate once the number is guessed
+
+
+// let x= Math.random() *100;
+// x= Math.round(x);
+
+// let inp;
+// let score =100;
+//  while (inp!= x) {
+//     score= score-1;
+//     inp= prompt('enter the number:');
+//     if (inp ==x ){
+//         console.log('congratulation you guess the correct answer');
+//         console.log(`you guess the correct number in  ${100-score} chances`)
+//     }
+//     else if (inp > x ){
+//         console.log('your number is greater than the actual number')
+//     }
+//     else if (inp < x ){
+//         console.log('your number is lesser than the actual number')
+//     }
+//     else{
+//         console.log('enter number between 1 to 100')
+//     }
+    
+//  }
+  
+
+
+
