7a8bf1cc6559630b4e4aedd98c7997aa2feae57a
Arun <kingarun2020@gmail.com>
2023-07-06 13:49:41 +0545

    revision

---
+++ part8.js
@@ -0,0 +1,71 @@
+// it shows differnt methods of console
+// console.log(console);  
+
+
+// console.table : is used to show data in tabular form
+// let obj= {
+//     a :1,
+//     b: 2,
+//     c: 3
+// }
+// console.table(obj); 
+
+// js alert,prompt and confirm
+// alert is used to show the alert message in the windows 
+// alert("Hello give the value of a");
+
+//prompt: it is used to take value as a string it can give another argument which is used as a default value
+// let a= prompt("enter the value of a ",'32') ;
+
+//confirm  it takes true false value 
+
+// let write = confirm("do you want to write to the page")
+// if (write){
+//     document.write(a)
+// }
+// else{
+//     document.write("ok no worries");
+// }
+
+//window object, BOM, DOM
+// console.log(window) // it is a global object
+
+//dom : document object model represent the content as HTML
+// console.log(document)
+// console.log(document.body)
+
+//bom : rep additional objects provided by the browser
+
+//rock ,paper ,scissor game:
+
+
+// let user = prompt("S P R");
+// let cpuI= Math.random()*3;
+//  cpuI=Math.floor(cpuI);
+// let cpu = ["R", "P","S"][cpuI]
+// if (user === cpu) {
+//     console.log('Match tied')
+    
+// }
+// else if(cpu ==='S' && user ==='P'){
+//     console.log('CPU WOn')
+// }
+// else if (cpu ==='S'&& user ==='R'){
+//     console.log("You Won")
+// }
+// else if(cpu ==='P' && user ==='S'){
+//     console.log('You WOn')
+// }
+// else if (cpu ==='P'&& user ==='R'){
+//     console.log("CPU Won")
+// }
+// else if(cpu ==='R' && user ==='P'){
+//     console.log('YOU WOn')
+// }
+// else if (cpu ==='R'&& user ==='S'){
+//     console.log("CPU Won")
+// }
+// console.log(`CPU Choose: ${cpu} and you choose : ${user}`);
+
+
+
