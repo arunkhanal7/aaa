39bc3ba985d4c5df5948c2c9cb16c6c9569ebcb6
Arun <kingarun2020@gmail.com>
2023-07-12 15:22:39 +0545

    dom

--- part9.js
+++ part9.js
@@ -1,6 +1,15 @@
 // dom 
 // accessing first child and lastchild
 
-console.log(document.body.firstChild);
-console.log(document.body.lastChild);
-console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.childNodes);
+
+//element only navigation dom
+// let b= document.body
+
+// this gives first child
+// console.log("First child of b is :",b.firstChild); 
+
+// this gives first element child 
+// console.log("first element of b is :" ,b.firstElementChild) 
