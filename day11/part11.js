950bc407e3272bb6c70fa35aa05fd15ff42e07a6
Arun <kingarun2020@gmail.com>
2023-07-18 22:10:12 +0545

    day11

---
+++ day11/part11.js
@@ -0,0 +1,36 @@
+// set timeout and set interval
+
+// **set timeout allows us to run a function once after the interval of time
+
+// let a = setTimeout(() => {
+//     document.write("hello");
+// }, 2000);
+
+// clearTimeout(a);  //this clearls the timeout and stop it from execution
+
+
+//** set timeout with functions and argument */
+// const add = (a,b)=>{
+//     console.log("the sum is :"+ (a+b));
+//     a+b;
+// }
+// setTimeout(add,2000,2,3);
+
+
+//set interval: run after this second and tyo time paxi feri run hunxa yo process chalirahanxa
+//  let a =setInterval(() => {
+//     alert("hi I am set interval")
+// }, 3000);
+// clearInterval(a) // it stops the execution
+
+//events
+
+//onclick
+// let a = document.getElementsByClassName("btn")[0];
+// a.onclick =()=>{
+//     console.log("button is clicked")
+// }
+
+
+
+
