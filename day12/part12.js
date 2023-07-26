12a7cee72cf728e9cf71524c83d79625f2178c7d
Arun <kingarun2020@gmail.com>
2023-07-26 23:32:32 +0545

    asynchronous and synchronous

---
+++ day12/part12.js
@@ -0,0 +1,14 @@
+//synchronus action: are the action that initiate and finish one by one line by line execute hunxa
+// let a = prompt("what is your name");
+// let b= prompt("what is your age");
+// console.log('My name is'+ a+ 'I am'+b+'years of old');
+
+//asynchronous action : are the action that we initiate now and they finishes later like settimeout
+
+// in this examplae start run then set timeout  initiated which gets in background and end runs and after 3 second hello world run
+
+// console.log("start")
+// setTimeout(() => {
+//     console.log("hello world")
+// }, 3000);
+// console.log("end")
