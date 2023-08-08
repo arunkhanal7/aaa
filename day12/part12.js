90fdde4adbe6ffcb19e47af1fbd35cf329ad8764
Arun <kingarun2020@gmail.com>
2023-08-08 14:52:10 +0545

    promises

--- day12/part12.js
+++ day12/part12.js
@@ -64,3 +64,89 @@
 // loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",hello);
 
 //error handling also can be done 
+
+
+//promise 
+//simple example
+
+// let p = new Promise((resolve,reject)=>{
+//     let a= 1+1;
+//     if(a==2){
+//         resolve('Success')
+//     }
+//     else{
+//         reject('failed')
+//     }
+// })
+// p.then((message)=>{
+//     console.log('This is in the then '+ message)
+// }).catch((message)=>{
+//     console.log('This is iin the catch'+ message)
+// })
+
+//promises can be consumed by .then and .catch methods
+
+
+// promise then() method is invoked when promise is either resolved or rejected
+// Parameters: It takes two functions as parameters.
+
+//     The first function is executed if the promise is resolved and a result is received.
+//     The second function is executed if the promise is rejected and an error is received. (It is optional and there is a better way to handle error using .catch() method
+
+// Syntax:
+
+// .then(function(result){
+//         //handle success
+// }, function(error){
+//         //handle error
+// })
+
+//Example 2: This example shows how the then method handles when a promise is resolved
+
+// let promise = new Promise(function(resolve,reject){
+//     resolve('Success')
+// })
+// promise.then(function(successmess){
+//     console.log(successmess)
+// },function(errormess){
+//     console.log(errormess)
+// });
+
+//Example 3: This example shows the condition when a rejected promise is handled by second function of then method
+
+// let promise = new Promise(function(resolve,reject){
+//     reject('Promise Rejected')
+// })
+// promise.then((message)=>{
+//     console.log(message);
+// },(errormessage)=>{
+//     console.log(errormessage);
+// })
+
+
+
+// 2. Promise catch() Method: It is invoked when a promise is either rejected or some error has occurred in execution. It is used as an Error Handler whenever at any step there is a chance of getting an error.
+
+// Parameters: It takes one function as a parameter.
+
+//     Function to handle errors or promise rejections.(.catch() method internally calls .then(null, errorHandler), i.e. .catch() is just a shorthand for .then(null, errorHandler) )
+
+// Syntax:
+
+// .catch(function(error){
+//         //handle error
+//     })
+
+//Examples 4: This example shows the catch method handling the reject function of promise.
+
+// let promise = new Promise ((resolve,reject)=>{
+//     reject('Promise is rejected')
+// })
+// promise.then((successmessage)=>{
+//     console.log(successmessage);
+// })
+// .catch((errormessage)=>{
+//     console.log(errormessage);
+// })
+
+// https://blog.yogeshchavan.dev/the-most-important-thing-you-dont-know-about-promises yo herda hunxa ek taal
